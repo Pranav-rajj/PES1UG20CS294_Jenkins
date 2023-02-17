6451264b40c14f05920237b22bbca111d66cbafa
Pranav-rajj <77331018+Pranav-rajj@users.noreply.github.com>
2023-02-17 10:11:33 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,6 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "Task-4-PES1UG20CS294\n";
   return 0;
 }
